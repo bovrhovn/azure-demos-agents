ca6e346b506726b81c5dd9a84c1fab88c92bb797
Bojan Vrhovnik <bojan@vrhovnik.net>
2026-04-14 08:50:05 +0200

    Add SimpleAgent with background responses and Azure AI integration

---
+++ src/MAF/MAF.SimpleAgent.BackgroundResponses/Program.cs
@@ -0,0 +1,44 @@
+﻿using Azure.AI.Projects;
+using Azure.Identity;
+using Microsoft.Agents.AI;
+using Spectre.Console;
+
+AnsiConsole.MarkupLine("[blue]Simple Agent With Background Responses[/]");
+
+#region ENV variables
+
+var endpoint = Environment.GetEnvironmentVariable("ENDPOINT");
+ArgumentException.ThrowIfNullOrEmpty(endpoint, "ENDPOINT environment variable is not set.");
+var deploymentName = Environment.GetEnvironmentVariable("DEPLOYMENTNAME");
+ArgumentException.ThrowIfNullOrEmpty(deploymentName, "DEPLOYMENTNAME environment variable is not set.");
+
+AnsiConsole.MarkupLine("[blue]Endpoint [/]: " + endpoint);
+AnsiConsole.MarkupLine("[blue]Deployment Name[/]: " + deploymentName);
+
+#endregion
+
+AIAgent agent = new AIProjectClient(new Uri(endpoint), new DefaultAzureCredential())
+    .AsAIAgent(
+        model: deploymentName,
+        instructions: "You are a friendly assistant.",
+        name: "SimpleAgentWithBackgrondResponses");
+var question = AnsiConsole.Ask<string>("Ask your question",
+    "do long poem about development in the style of Lord of the Rings");
+AnsiConsole.MarkupLine("[green]Question:[/]" + question);
+AgentRunOptions options = new()
+{
+    AllowBackgroundResponses = true
+};
+
+AgentSession session = await agent.CreateSessionAsync();
+AnsiConsole.MarkupLine("[green]Session created, creating long poem.[/]");
+AgentResponse response = await agent.RunAsync(question, session, options);
+// Continue to poll until the final response is received
+while (response is { ContinuationToken: { } token })
+{
+    // Wait before polling again.
+    await Task.Delay(TimeSpan.FromSeconds(2));
+    options.ContinuationToken = token;
+    response = await agent.RunAsync(session, options);
+}
+AnsiConsole.MarkupLine("[green]Long poem done.[/]");
